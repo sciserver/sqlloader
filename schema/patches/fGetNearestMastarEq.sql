fe8c877424650090bbf352bf40d051d0bb0fd280
suzanne werner <capnsue@gmail.com>
2018-10-30 12:42:55 -0400

    updated documentation for mastar functions

--- schema/patches/fGetNearestMastarEq.sql
+++ schema/patches/fGetNearestMastarEq.sql
@@ -13,20 +13,11 @@ CREATE FUNCTION [dbo].[fGetNearestMastarEq] (@ra float, @dec float, @r float)
 -------------------------------------------------------------
 --/T There is no limit on the number of objects returned, but there are about 40 per sq arcmin.
 --/T <p>returned table:  
---/T <li> plateIFU varchar(20) NOT NULL,   -- MaNGA unique ID
 --/T <li> mangaid varchar(20) NOT NULL,    -- MaNGA target id
 --/T <li> objra float NOT NULL,            -- Right Ascension
 --/T <li> objdec float NOT NULL,	   -- declination
---/T <li> ifura float NOT NULL,            -- Right Ascension
---/T <li> ifudec float NOT NULL,	   -- declination
---/T <li> drp3qual bigint NOT NULL,	   -- Quality bitmask
---/T <li> bluesn2 real NOT NULL, 	   -- Total blue SN2 across all nexp exposures
---/T <li> redsn2 real NOT NULL,	   -- Total red SN2 across all nexp exposures
---/T <li> mjdmax int NOT NULL,	   	   --/D Maximum MJD across all exposures
---/T <li> mngtarg1 bigint NOT NULL,	   --/D Manga-target1 maskbit for galaxy target catalog
---/T <li> mngtarg2 bigint NOT NULL,	   --/D Manga-target2 maskbit for galaxy target catalog
---/T <li> mngtarg3 bigint NOT NULL,	   --/D Manga-target3 maskbit for galaxy target catalog
---/T <li> htmID bigint NOT NULL		   -- Hierarchical Trangular Mesh id of this objetc
+--/T <li> htmID bigint NOT NULL		   -- Hierarchical Trangular Mesh id of this object
+--/T <li> distance float NOT NULL		-- distance in arc minutes to this object from the ra,dec.
 --/T <br> Sample call to find MaNGA object within 5 arcminutes of xyz -.0996,-.1,0
 --/T <br><samp>
 --/T <br>select * from dbo.[fGetNearestMastarEq](38.7, 47.4, 1) 
