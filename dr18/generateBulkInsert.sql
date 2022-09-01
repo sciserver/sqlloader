10fc37317c0e86cf9049faff027129593b171998
suzanne werner <capnsue@gmail.com>
2022-09-01 15:27:37 -0400

    script to generate/run bulk inserts

---
+++ dr18/generateBulkInsert.sql
@@ -0,0 +1,64 @@
+
+-- ###################
+-- 
+-- load dr18 csv files 
+--
+-- ####################
+
+--select * from filesToLoad2
+
+
+declare @doExecute bit = 1
+declare @truncate bit = 1
+
+declare @sql nvarchar(4000)
+declare @id int
+declare @fname nvarchar(2000)
+declare @targettable sysname
+
+declare cur cursor fast_forward for 
+select id, fname, targettable from filestoload2
+
+
+open cur
+fetch next from cur into @id, @fname, @targettable
+while @@FETCH_STATUS = 0
+begin
+/*
+BULK INSERT Sales.Orders
+FROM '\\SystemX\DiskZ\Sales\data\orders.csv'
+WITH ( FORMAT = 'CSV');
+*/
+	set @sql = ''
+	if @truncate = 1
+		set @sql = concat('TRUNCATE TABLE ',@targettable,';')
+	--BULK INSERT dr18_allwise FROM 'd:\dr18loading\minidb\csv\minidb.dr18_allwise.csv' WITH (DATAFILETYPE='char', FIRSTROW=2, FIELDTERMINATOR=',', rowterminator='0x0a');
+
+	
+	set @sql = concat(@sql, 'BULK INSERT ', @targettable, ' FROM ''', @fname, ''' WITH (DATAFILETYPE=''char'', FIRSTROW=2, FIELDTERMINATOR='','', ROWTERMINATOR=''0x0a'');','
+	')
+	print @sql
+	if (@doExecute = 1)
+	begin
+		
+			exec sp_executesql @sql
+	end
+
+	set @sql = concat('update filestoload2 set lastLoadDate=''',GETDATE(),''', loadStatus=1 WHERE id=',@id,'
+	')
+	print @sql
+	if (@doExecute = 1)
+	begin
+		
+			exec sp_executesql @sql
+	end
+	
+	fetch next from cur into @id, @fname, @targettable
+	
+end
+
+close cur
+deallocate cur
+
+
+
